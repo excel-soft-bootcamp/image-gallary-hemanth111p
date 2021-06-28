9ca3522beed3565ad27b44a1068ee5425c61e326
hemanth111p <72683647+hemanth111p@users.noreply.github.com>
2021-06-28 20:25:00 +0530

    uploaded

---
+++ ImageGallery/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace image
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            ImageGallery GmailShare = new ImageGallery(new Gmail());
+            GmailShare.Share();
+            ImageGallery WhatsUpShare = new ImageGallery(new WhatsUp());
+            WhatsUpShare.Share();
+            ImageGallery BluetoothShare = new ImageGallery(new Bluetooth());
+            BluetoothShare.Share();
+
+
+
+        }
+    }
+}
